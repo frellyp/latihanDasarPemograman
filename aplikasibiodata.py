848f296223acf90ed603cb39649aeb9883b5d7c3
frellyp <114639326+frellyp@users.noreply.github.com>
2022-09-29 11:44:24 +0700

    Add files via upload

---
+++ aplikasibiodata.py
@@ -0,0 +1,10 @@
+# Aplikasi biodata create.by frelly
+
+nama= input("masukan nama anda ")
+print("nama anda adalah ;  ",nama)
+
+kelas= input("masukan kelas anda ")
+print("kelas anda adalah ;  ",kelas)
+
+prodi= input("masukan prodi anda ")
+print("prodi anda adalah ;  ",prodi)
